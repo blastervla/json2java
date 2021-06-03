92e2132be16a2f9b29e3661a21f560c72761e550
Vladimir Pomsztein <vladimir.pomsztein@mercadolibre.com>
2021-06-03 19:04:36 -0300

    Refactor: Legibility++

--- json2java.py
+++ json2java.py
@@ -1,15 +1,13 @@
-# You can get the schema at https://www.liquid-technologies.com/online-json-to-schema-converter
-# Use "List Validation" in Array rules setting
-
 import json
 from genson import SchemaBuilder
 
+
 def getClassName(snakeStr):
     camel = toLowerCamelCase(snakeStr)
-    name = camel.title()[0] + camel[1:]
-    if name == "List" or name == "Object":
-        name = "Item" + name
-    return name
+    className = camel.title()[0] + camel[1:]
+    if className == "List" or className == "Object":
+        className = "Item" + className
+    return className
 
 
 def toLowerCamelCase(snakeStr):
@@ -17,36 +15,39 @@ def toLowerCamelCase(snakeStr):
     return components[0] + ''.join(x.title() for x in components[1:])
 
 
+# Didn't give much thought to this, it's pretty ugly
+# TODO: Refactor and give this a bit more love
 def mostGenericType(typesList):
     mostGeneric = typesList[0]
-    for type in typesList:
+    for candidateType in typesList:
         if mostGeneric == 'string':
-            if type == 'number' or type == 'integer':
+            if candidateType == 'number' or candidateType == 'integer':
                 mostGeneric = 'null'
             else:
                 mostGeneric = 'string'
         elif mostGeneric == 'integer':
-            if type == 'number':
+            if candidateType == 'number':
                 mostGeneric = 'number'
-            elif type == 'integer':
+            elif candidateType == 'integer':
                 mostGeneric = 'integer'
             else:
                 mostGeneric = 'null'
         elif mostGeneric == 'number':
-            if type == 'number' or 'integer':
+            if candidateType == 'number' or 'integer':
                 mostGeneric = 'number'
             else:
                 mostGeneric = 'null'
         elif mostGeneric == 'object':
             mostGeneric = 'object'
         else:
-            if type == 'object':
+            if candidateType == 'object':
                 mostGeneric = 'object'
             else:
                 mostGeneric = 'null'
     return mostGeneric
 
 
+# noinspection PyShadowingNames
 def recursiveJson2Java(schema, name, isTopLevel, topLevelOnly, levelIndent):
     if "type" in schema:
         schemaType = schema["type"]
@@ -58,65 +59,69 @@ def recursiveJson2Java(schema, name, isTopLevel, topLevelOnly, levelIndent):
         schemaType = mostGenericType(schemaType)
 
     if schemaType == "object":
-        className = getClassName(name)
-        res = ""
-        if "properties" in schema and (not topLevelOnly or isTopLevel):
-            res += levelIndent + "public " + ("" if isTopLevel else "static ") + "class " + className + " {\n"
-            for prop in schema["properties"]:
-                res += recursiveJson2Java(schema["properties"][prop], prop, False, topLevelOnly, levelIndent + "    ")
-            res += levelIndent + "}\n"
-        else:
-            className = "Map<String, Object>"
-
-        if not isTopLevel:
-            res += levelIndent + "public " + className + " " + toLowerCamelCase(name) + ";\n"
-
-        return res
+        return toJavaObject(schema, name, isTopLevel, topLevelOnly, levelIndent)
     elif schemaType == "array":
-        if "items" not in schema or schema["items"] is None or "type" not in schema["items"]:
-            itemsType = 'null'
-        else:
-            itemsType = schema["items"]["type"]
+        return toJavaList(schema, name, topLevelOnly, levelIndent)
+    else:  # primitive
+        return toJavaPrimitivesOrThrowError(schemaType, name, levelIndent)
 
-        if itemsType == "object":
-            className = getClassName(name.rstrip("s"))
-            res = levelIndent + "public List<" + className + "> " + toLowerCamelCase(name) + ";\n"
-            if not topLevelOnly:
-                recRes = recursiveJson2Java(schema["items"], className, False, topLevelOnly, levelIndent)
-                recRes = '\n'.join(recRes.split('\n')[:-2]) + "\n"
-                res += recRes
-            return res
-        elif itemsType == "string":
-            return levelIndent + "public List<String> " + toLowerCamelCase(name) + ";\n"
-        elif itemsType == "integer":
-            return levelIndent + "public List<Integer> " + toLowerCamelCase(name) + ";\n"
-        elif itemsType == "number":
-            return levelIndent + "public List<Float> " + toLowerCamelCase(name) + ";\n"
-        elif itemsType == "boolean":
-            return levelIndent + "public List<Boolean> " + toLowerCamelCase(name) + ";\n"
-        elif itemsType == "null":
-            return levelIndent + "public List<Object> " + toLowerCamelCase(name) + "; // Was null in JSON\n"
-        else:
-            print("UNKNOWN TYPE '" + itemsType + "' in array")
-            return "ERROR\n"
-    elif schemaType == "string":
+
+# noinspection PyShadowingNames
+def toJavaList(schema, name, topLevelOnly, levelIndent):
+    if "items" not in schema or schema["items"] is None or "type" not in schema["items"]:
+        itemsType = 'null'
+    else:
+        itemsType = schema["items"]["type"]
+    if itemsType == "object":
+        className = getClassName(name.rstrip("s"))
+        res = levelIndent + "public List<" + className + "> " + toLowerCamelCase(name) + ";\n"
+        if not topLevelOnly:
+            recRes = recursiveJson2Java(schema["items"], className, False, topLevelOnly, levelIndent)
+            recRes = '\n'.join(recRes.split('\n')[:-2]) + "\n"
+            res += recRes
+        return res
+    else:
+        return toJavaPrimitivesOrThrowError(itemsType, name, levelIndent)
+
+
+# noinspection PyShadowingNames
+def toJavaPrimitivesOrThrowError(primitiveType, name, levelIndent):
+    if primitiveType == "string":
         return levelIndent + "public String " + toLowerCamelCase(name) + ";\n"
-    elif schemaType == "integer":
+    elif primitiveType == "integer":
         return levelIndent + "public Integer " + toLowerCamelCase(name) + ";\n"
-    elif schemaType == "number":
+    elif primitiveType == "number":
         return levelIndent + "public Float " + toLowerCamelCase(name) + ";\n"
-    elif schemaType == "boolean":
+    elif primitiveType == "boolean":
         return levelIndent + "public Boolean " + toLowerCamelCase(name) + ";\n"
-    elif schemaType == "null":
+    elif primitiveType == "null":
         return levelIndent + "public Object " + toLowerCamelCase(name) + "; // Was null in JSON\n"
     else:
-        print("UNKNOWN TYPE '" + schemaType + "'")
+        print("UNKNOWN TYPE '" + primitiveType + "'")
         return "ERROR\n"
 
 
+# noinspection PyShadowingNames
+def toJavaObject(schema, name, isTopLevel, topLevelOnly, levelIndent):
+    className = getClassName(name)
+    res = ""
+    if "properties" in schema and (not topLevelOnly or isTopLevel):
+        res += levelIndent + "public " + ("" if isTopLevel else "static ") + "class " + className + " {\n"
+        for prop in schema["properties"]:
+            res += recursiveJson2Java(schema["properties"][prop], prop, False, topLevelOnly, levelIndent + "    ")
+        res += levelIndent + "}\n"
+    else:
+        className = "Map<String, Object>"
+    if not isTopLevel:
+        res += levelIndent + "public " + className + " " + toLowerCamelCase(name) + ";\n"
+    return res
+
+
+# ====== MAIN FLOW ====== #
+
 name = input("Gimme a name (ANY name) for the Java class\n")
 
-isTopLevelOnly = "S" in input("Would you like to have a Shallow (Maps) or Deep (Model objects) model?\n")
+isTopLevelOnly = "S" in input("Would you like to have a [S]hallow (Maps) or [D]eep (Model objects) model?\n")
 
 lines = ""
 x = input("Gimme a json, and I'll do the magic\n")
